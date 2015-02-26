4e057ca75fe6139771bdee909ba442b98bc0fa04
angela riggs <riggs.ang@gmail.com>
2015-02-25 17:06:49 -0800

    working js for off canvas nav, credited source

--- assets/javascript/off-canvas-menu.js
+++ assets/javascript/off-canvas-menu.js
@@ -4,9 +4,13 @@
 
 })*/ 
 
+/* JavaScript code tutorial from Tom Rich
+@ http://blog.tomri.ch/super-simple-off-canvas-menu-navigation/ */
+
 $(".menu-button").click(function() {
   $("#menu").toggleClass("active");
   $("#container").toggleClass("active");
   $("#header").toggleClass("active");
+
   console.log("yay");
 });
